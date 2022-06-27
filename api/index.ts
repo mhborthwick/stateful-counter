d9351ebeb0511de654a3add3dc1bbd6a788d8ef0
mhborthwick <mhborthwick@gmail.com>
2022-06-26 18:03:21 -0700

    chore: set up express

---
+++ api/index.ts
@@ -0,0 +1,46 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
+app.use(cookieParser());
+
+/**
+ * Check server health
+ */
+app.get("/", (_, res) => {
+  res.send("Health Check: GOOD");
+});
+
+/**
+ * Get user
+ */
+app.get("/user", async (req, res) => {});
+
+/**
+ * Get count
+ */
+app.get("/count", async (req, res) => {});
+
+/**
+ * Increment count
+ */
+app.post("/increment", async (req, res) => {});
+
+/**
+ * Decrement count
+ */
+app.post("/decrement", async (req, res) => {});
+
+/**
+ * Reset count
+ */
+app.post("/reset", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
